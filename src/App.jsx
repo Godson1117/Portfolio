e2fd03e13977bf1dde594b085fd72b0c5ded7459
Godson Sojan <91582855+Godson1117@users.noreply.github.com>
2024-07-02 17:26:00 +0000

    Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Godson Sojan",
+  title: "Junior Web Developer",
+  email: "godsonsojan@gmail.com",
+  gitHub: "Godson1117",
+  instagram: "godson_sojan",
+  linkedIn: "Godson1117",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
